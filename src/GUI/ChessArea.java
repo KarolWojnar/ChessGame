3c9b9f57d29a6a7f661190f53c0fc2fe3ba7c927
KarolWojnar <Wojnar@12345>
2023-03-01 20:34:57 +0100

    1 commm

--- src/GUI/ChessArea.java
+++ src/GUI/ChessArea.java
@@ -1,4 +1,23 @@
 package GUI;
 
-public class ChessArea {
+import javax.swing.*;
+import java.awt.*;
+
+public class ChessArea extends JFrame {
+
+
+    public static void main(String[] args) {
+        new ChessArea().setVisible(true);
+    }
+    ChessArea() {
+        initArea();
+    }
+    public void initArea(){
+        this.setTitle("Chess");
+        this.setIconImage(new ImageIcon("ImIc.jpg").getImage());
+        int width = Toolkit.getDefaultToolkit().getScreenSize().width / 2;
+        int height = Toolkit.getDefaultToolkit().getScreenSize().height / 2;
+        this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        this.setBounds(width / 2, height / 4, 800,850);
+    }
 }
